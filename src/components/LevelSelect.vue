38c3f20e1f11ee5a3cf66efba385ed6dac896da8
Martinez <amartin5@th-koeln.de>
2024-06-16 17:05:05 +0200

    Reworked Level Select
    TODO Tutorial Area Clickable machen und Tutorials einbauen (Wird wahrscheinlich erst einmal ein Work in Progress Feature bleiben

--- src/components/LevelSelect.vue
+++ src/components/LevelSelect.vue
@@ -16,10 +16,9 @@ export default {
       <button class="bg-yellow-500 rounded-t-lg w-1/6 h-16">Tutorials</button>
     </div>
     
-    <div class="flex flex-col border-2 w-3/4 h-full rounded-3xl items-center">
-      <router-link v-for="level in levels" :key="`${level.main_chapter}-${level.sub_chapter}`" :to="`/levels/${level.main_chapter}/chapters/${level.sub_chapter}`" class="w-full">
-        <LevelSelectButton :level="level" />
-      </router-link>
+    <div class="bg-red-500 flex flex-col w-3/4 rounded-3xl items-center">
+      <!--  <router-link v-for="level in levels" :key="`${level.main_chapter}-${level.sub_chapter}`" :to="`/levels/${level.main_chapter}/chapters/${level.sub_chapter}`" class="w-full"> -->
+      <LevelSelectButton v-for="level in levels" :level="level" />
     </div>
   </div>
 </template>
--- src/components/LevelSelectButton.vue
+++ src/components/LevelSelectButton.vue
@@ -21,25 +21,33 @@ export default {
 </script>
 
 <template>
-    <div class="bg-red-300 h-24 sm:h-32 md:h-40 lg:h-48 xl:h-56 w-11/12 mx-auto my-6 rounded flex items-center justify-center">
-        <button class="w-full h-full flex items-center justify-between m-4">
-            <div class="w-1/6 h-5/6" :style="backgroundStyle"></div>
-            <div class="flex flex-row">
-              <div class="flex flex-col">
-                <div>
-                  {{ props.level.title }}
-                </div>
-                <div>
-                  {{ props.level.preview.description }}
-                </div>
-              </div>
-              
-              <div class="text-4xl">
-                {{ props.level.main_chapter }}.{{ props.level.sub_chapter }}
-              </div>
+    <div class="bg-red-300 h-24 sm:h-32 md:h-40 lg:h-48 xl:h-56 w-11/12 mx-auto my-6 rounded-xl flex items-center justify-center">
+      <router-link :to="`/levels/${level.main_chapter}/chapters/${level.sub_chapter}`" class="w-full h-full flex items-center">
+        <div class="w-full h-full flex items-center m-4">
+            <div class="w-1/6 h-5/6 bg-gray-200 rounded-xl" :style="backgroundStyle">
+              <!-- Hier kommt das Preview Image rein -->
             </div>
-            
-        </button>
+
+            <div class="flex flex-col items-start ml-8 w-full h-full justify-around">
+                <div>
+                  <div class="text-4xl font-bold mb-2">
+                    {{ props.level.title }}
+                  </div>
+
+                  <div class="text-xl">
+                    {{ props.level.preview.description }}
+                  </div>
+                </div>
+
+                <div class="text-gray-600">
+                  Keywords:
+                  <span v-for="(flag, index) in level.flags" :key="flag.id">
+                    {{ flag.keyword }}<span v-if="index < level.flags.length - 1">, </span>
+                  </span>
+                </div>
+            </div>
+          </div>
+        </router-link>
     </div>
 </template>
 
